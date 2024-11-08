edcd2b0f19c47eea7c7f10b34d4847c66884e311
Cleberton <clebertonfgc@gmail.com>
2024-11-08 16:47:27 -0300

    add DTOs

---
+++ Restaurant.Application/DTOS/MenuItemDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Restaurant.Domain.Entities;
+
+namespace Restaurant.Application.DTOS;
+
+public class MenuItemDto
+{
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Name is required!")]
+    [MinLength(3)]
+    [MaxLength(100)]
+    [DisplayName("Name")]
+    public string? Name { get; set; }
+    
+    [Required(ErrorMessage = "Description is required!")]
+    [MinLength(5)]
+    [MaxLength(200)]
+    [DisplayName("Description")]
+    public string? Description { get; set; }
+    
+    [Required(ErrorMessage = "Price is required!")]
+    [Column(TypeName = "decimal(18,2)")]
+    [DisplayFormat(DataFormatString = "{0:C2}")]
+    [DataType(DataType.Currency)]
+    [DisplayName("Price")]
+    public decimal Price { get; set; }
+    
+    public bool Available { get; set; } = default;
+    
+    public Order? Order { get; set; }
+}
---
+++ Restaurant.Application/DTOS/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Restaurant.Domain.Entities;
+
+namespace Restaurant.Application.DTOS;
+
+public class OrderDto
+{
+    public DateTime Date { get; set; }
+    
+    [Required(ErrorMessage = "Name is required!")]
+    [MinLength(3)]
+    [MaxLength(100)]
+    [DisplayName("Name")]
+    public int TableNumber { get; set; }
+    public decimal Total { get; set; }
+}
